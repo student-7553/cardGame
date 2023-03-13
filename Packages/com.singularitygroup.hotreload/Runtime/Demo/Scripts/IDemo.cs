a39dc9f147ccdd5fa541aa9c5d35a2688427a632
Bilguun <soproatlol@gmail.com>
2023-03-13 13:17:08 +0800

    commit

---
+++ Packages/com.singularitygroup.hotreload/Runtime/Demo/Scripts/IDemo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SingularityGroup.HotReload.Demo {
+    public interface IDemo {
+        bool IsServerRunning();
+        void OpenHotReloadWindow();
+        void OpenScriptFile(TextAsset textAsset, int line, int column);
+    }
+    
+    public static class Demo {
+        public static IDemo I = new FallbackDemo();
+    }
+    
+    public class FallbackDemo : IDemo {
+        public bool IsServerRunning() {
+            return true;
+        }
+
+        public void OpenHotReloadWindow() {
+            //no-op
+        }
+
+        public void OpenScriptFile(TextAsset textAsset, int line, int column) {
+            //no-op
+        }
+    }
+}
