dcd0c4b6f4a1d600299231ffc7b61c0f3caf8066
Bilguun <soproatlol@gmail.com>
2024-02-03 16:01:22 +0800

    Added more buttons

--- Assets/Scripts/UI/UI_ZoomInButton.cs
+++ Assets/Scripts/UI/UI_ZoomInButton.cs
@@ -3,13 +3,34 @@ using UnityEngine.EventSystems;
 
 public class UI_ZoomInButton : EventTrigger
 {
+	public CameraController cameraController;
+
+	private void OnEnable()
+	{
+		init();
+	}
+
+	private void init()
+	{
+		// We could make this better but we won't :p
+		cameraController = (CameraController)FindObjectOfType(typeof(CameraController));
+	}
+
 	public override void OnPointerDown(PointerEventData data)
 	{
-		Debug.Log("OnPointerDown called.");
+		if (cameraController == null)
+		{
+			return;
+		}
+		cameraController.zoomAcceleration(1);
 	}
 
 	public override void OnPointerUp(PointerEventData data)
 	{
-		Debug.Log("OnPointerUp called.");
+		if (cameraController == null)
+		{
+			return;
+		}
+		cameraController.zoomAcceleration(0);
 	}
 }
